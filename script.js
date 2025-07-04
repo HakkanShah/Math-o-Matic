3af5cfff8e1a7f76d47454d807bf191bb58e4049
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-07-04 19:00:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -263,7 +263,7 @@ tabButtons.forEach(tab => {
             tabButtons.forEach(t => t.classList.remove('active'));
             tab.classList.add('active');
             
-            // Hide all sections first
+            
             standardButtons.classList.remove('active');
             scientificButtons.classList.remove('active');
             document.querySelector('.buttons.converter').classList.remove('active');
